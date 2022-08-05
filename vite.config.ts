83f8f18aac15459c2cfbef8bef64ed1bf3002360
Victor Batalha <victor.batalha@platformbuilders.io>
2022-08-05 20:21:45 -0300

    add plugin to allow top level await

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    topLevelAwait({
+      promiseExportName: '__tla',
+      promiseImportName: i => `__tla_${i}`,
+    }),
+  ],
 });
