96a3c3b3b3dc27b2541606908658c74c165c8b94
Victor Batalha <madivcb@gmail.com>
2022-11-24 19:22:48 -0300

    ssh key test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/spotify-clone/',
   build: {
